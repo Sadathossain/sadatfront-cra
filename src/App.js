67dcb47561a2d7fee8e9634b271020d948e525e4
Sadat Hossain <sadat.hossain@hotmail.com>
2019-03-21 23:23:31 +0600

    Added firebase-auth with google on main screen

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// firebase auth
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
 
 class App extends Component {
   render() {
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {
+            user
+              ? <p>Hello, {user.displayName}</p>
+              : <p>Please sign in.</p>
+          }
+          {
+            user
+              ? <button onClick={signOut}>Sign out</button>
+              : <button onClick={signInWithGoogle}>Sign in with Google</button>
+          }
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyApvBO_siRJ4p5DfwNokrDA1NElT-eXdvg",
+    authDomain: "cloud3-3duolc.firebaseapp.com",
+    databaseURL: "https://cloud3-3duolc.firebaseio.com",
+    projectId: "cloud3-3duolc",
+    storageBucket: "cloud3-3duolc.appspot.com",
+    messagingSenderId: "914451008595"
+};
+export default config;
